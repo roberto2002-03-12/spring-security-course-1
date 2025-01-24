4bd795eb22ebfab16d725157750a2e5ebb15074e
roberto2002-03-12 <robertog-18@hotmail.com>
2025-01-24 16:40:10 -0500

    feat: crear clase error invalid password

--- src/main/java/com/cursos/api/springsecuritycourse/exception/InvalidPasswordException.java
+++ src/main/java/com/cursos/api/springsecuritycourse/exception/InvalidPasswordException.java
@@ -1,4 +1,36 @@
 package com.cursos.api.springsecuritycourse.exception;
 
-public class InvalidPasswordException {
+public class InvalidPasswordException extends RuntimeException {
+    private final String password;
+    private final String passwordRepeated;
+    private final String errorDescription;
+
+    public InvalidPasswordException(String password, String errorDescription) {
+        this.password = password;
+        this.passwordRepeated = password;
+        this.errorDescription = errorDescription;
+    }
+
+    public InvalidPasswordException(String password, String passwordRepeated, String errorDescription) {
+        this.password = password;
+        this.passwordRepeated = passwordRepeated;
+        this.errorDescription = errorDescription;
+    }
+
+    @Override
+    public String getMessage() {
+        return "Invalid password: " + this.errorDescription;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPasswordRepeated() {
+        return passwordRepeated;
+    }
+
+    public String getErrorDescription() {
+        return errorDescription;
+    }
 }
